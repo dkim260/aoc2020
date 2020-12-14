b4c654015cc1d2c77c20c6767bae4eb84848541c
dkim260 <d.dkim026@gmail.com>
2020-12-14 14:00:22 -0500

    revisited high school vectors

--- 12/12a.php
+++ 12/12a.php
@@ -20,10 +20,10 @@
 
         public function goL($units){ //Feel like this is cheating, but problem was a bit hard to understand
             if ($units==90){
-                //10e 4n <- 4e 10s
-                $temp = $this->y;
-                $this->y=$this->x;
-                $this->x=$temp*-1;
+                $x = $this->x;
+                $y = $this->y;
+                $this->x= $x * cos(deg2rad(($units))) - $y * sin(deg2rad($units));
+                $this->y= $x * sin(deg2rad($units)) + $y * cos(deg2rad($units));
             }
             else if ($units==180){
                 $this->x = $this->x * -1;
@@ -44,18 +44,12 @@
                 $this->x = $this->y;
                 $this->y = $temp * -1;*/
                 //10e 4n -> 4e 10s
-                if ($this->x>=0 && $this->y>=0){
-                    $temp = $this->x;
-                    $this->x = $this->y;
-                    $this->y = $temp * -1;
-                }
-                else if ($this->x>=0 && $this->y<0){//Double check TODO ****************
-                }
-                else if ($this->y<0 && $this->x<0){
-                }
-                else{
-                }
 
+                //Rotation matrix:
+                $x = $this->x;
+                $y = $this->y;
+                $this->x=  round ($x * cos(deg2rad(($units*-1))) - $y * sin(deg2rad($units*-1)));
+                $this->y=  round ($x * sin(deg2rad($units*-1)) + $y * cos(deg2rad($units*-1)));
             }
             else if ($units==180){
                 $this->x = $this->x * -1;
@@ -74,13 +68,13 @@
                 $this->ship->goE($units*$this->x);
             }
             else{// ($this->x<0)
-                $this->ship->goW($units*$this->x);
+                $this->ship->goW($units*$this->x*-1);
             }
             if ($this->y>=0){
                 $this->ship->goN($units*$this->y);
             }
             else{
-                $this->ship->goS($units*$this->y);
+                $this->ship->goS($units*$this->y*-1);
             }
         }
 
@@ -164,9 +158,7 @@
     }
 
     $wp = new waypoint;
-    var_dump($wp);
 
-    /*
     $parse = fopen("input.txt", "r");
 
     
@@ -176,20 +168,21 @@
         $command = $input[0];
         $num = $input[1];
 
-        if ($command=="N"){$ship->goN($num);}
-        else if ($command=="E"){$ship->goE($num);}
-        else if ($command=="S"){$ship->goS($num);}
-        else if ($command=="W"){$ship->goW($num);}
-        else if ($command=="F"){$ship->goF($num);}
-        else if ($command=="L"){$ship->turnL($num);}
-        else if ($command=="R"){$ship->turnR($num);}
+        if ($command=="N"){$wp->goN($num);}
+        else if ($command=="E"){$wp->goE($num);}
+        else if ($command=="S"){$wp->goS($num);}
+        else if ($command=="W"){$wp->goW($num);}
+        else if ($command=="F"){$wp->goF($num);}
+        else if ($command=="L"){$wp->goL($num);}
+        else if ($command=="R"){$wp->goR($num);}
         else{printf("Something went wrong reading input");}
+        //var_dump($wp);
 
         $input = fscanf($parse, "%c%d");
     }
 
     fclose($parse);
 
-    printf ("Man sum: %d\n", $ship->manSum());
-*/
+    printf ("Man sum: %d\n", $wp->ship->manSum());
+
 ?>
