2242d0dcb79311869d924e9ad0fc9a7171a4bd4f
dkim26) <dkim260@github.com>
2021-03-13 16:52:57 -0500

    more brainstorming, relearning, wrangling

--- aoc/2020/16/16.php
+++ aoc/2020/16/16.php
@@ -17,10 +17,17 @@
         public $upper2;
     }
 
+    class node{
+        public $start; //type of rule
+        public $end;
+        public $connections = array(); //holds edges
+        public $connectVisits = array(); //holds number of visits from edges        
+
+    }
+
     class ticket{
         public $inputs = array(); //array
     }
-
 
     $file = fopen("input.txt", "r");
 
@@ -69,12 +76,43 @@
     }
     fclose($file);
 
+    $sections = array();
+
     //I want to roll out the bounds like a piece of string
+    //Or, maybe set up a graph and mark edges as visited
+
+    //split rules
     foreach ($rules as $rule){
+        $nod1 = new node;
+        $nod1->start = $rule->lower1;
+        $nod1->end = $rule->upper1;
+        
+        $nod2 = new node;
+        $nod2->start = $rule->lower2;
+        $nod2->end = $rule->upper2;
+
+        array_push($sections, $nod1);
+        array_push($sections, $nod2);
+    }
+
+    //add connections
+    foreach ($sections as $rule1){
+        foreach ($sections as $rule2){
+             if (!array_key_exists($rule2->start ,$rule1->connections) && $rule2->start <= $rule1->end){
+                 array_push($rule1->connections, $rule2);
+                 array_push($rule1->connectVisits, 0);
+             }
+        }
+    }
+
+    //sort the connections
+    foreach ($section as $sectionb){
 
     }
 
-    //Or, maybe set up a graph and mark edges as visited
+    //find the highest gap each ticket section can fit
     
+
+    return;
 
 ?>
