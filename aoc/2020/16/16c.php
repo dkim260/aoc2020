743c5fa552f3036d8fc45bbf7fed2e51257a5194
sheepduck <d.dkim026@gmail.com>
2021-05-10 00:10:44 -0400

    progress?

--- aoc/2020/16/16c.php
+++ aoc/2020/16/16c.php
@@ -173,54 +173,6 @@
         array_push($order, $x);
     }
 
-    //Sleep deprived chat with someone
-    //Each value columnwise for the tickets should be unique?
-    //Verify which rule(s?) works for a column
-    //Go back to original idea of sort by column
-
-    /*
-    $failedcount = 0;
-    for ($y = 0; $y < count($rules); $y++){
-
-        $flag = false;
-        $failed = array();
-        for ($x = 0; $x < count($tickets); $x++){
-            if (!(  ($tickets[$x][$y] >= $rules[$y]->lower1 &&  $tickets[$x][$y] <= $rules[$y]->upper1) ||  ($tickets[$x][$y] >= $rules[$y]->lower2 && $tickets[$x][$y] <= $rules[$y]->upper2)  )){
-                $flag = true;
-                //php arrays: only string or ints can be keys
-                /*
-                array_push($failed, $failed[$tickets[$x]]);
-                $failed[$tickets[$x]]=$failed[$x][$y];
-                */
-                /*
-                $failed[$x] = $tickets[$x][$y];
-            }
-        }
-
-        if ($flag === false){
-            printf("works\n");
-        }
-
-        else{
-            //printf("fails\n");
-            var_dump($failed);
-            var_dump($y);
-            var_dump($rules[$y]);
-            $failedcount++;
-        }
-    }*/
-
-    //Observations from above: Seems like it's only one ticket that will fail a rule
-    //10 rules fail
-    //Try swapping a rule with another and see the results
-    //10! is significantly smaller than 20! for brute forcing at least for testing
-
-    /*
-    $order[0]=1;
-    $order[1]=0;
-    Fails 
-    */
-
     //Shorter version
     function verify3($order){
         global $rules, $tickets;
@@ -232,102 +184,49 @@
                 }
             }
         }
-
         return true;
     }
 
-    $reductions = array();
-
-    $failed = failedrules($order);
-
-    function failedrules($order){
-        global $rules, $tickets;
-        $failed = array();
-
-        foreach ($order as $y){
-            for ($x = 0; $x < count($tickets); $x++){
-                if (!(  ($tickets[$x][$y] >= $rules[$y]->lower1 &&  $tickets[$x][$y] <= $rules[$y]->upper1) ||  ($tickets[$x][$y] >= $rules[$y]->lower2 && $tickets[$x][$y] <= $rules[$y]->upper2)  )){
-                    $failed[count($failed)] = $y;
+    //Let's write down which rules parts of a ticket satisfies
+    $satisfies = array();
+    for ($x = 0; $x<count($tickets); $x++){
+        $satisfies[$x] = array();
+        for ($y = 0; $y< 20; $y++){
+            $satisfies[$x][$y] = array();
+            $z=0;
+            foreach ($rules as $rule){
+                $satisfies[$x][$y][$z] = array();
+                if ($tickets[$x][$y] >= $rule->lower1 && $tickets[$x][$y]<= $rule->upper1){
+                    array_push($satisfies[$x][$y][$z],"a");
                 }
-            }
-        }
-        return $failed;
-    }
-
-    function swapAndVerify ($holder){
-        //swap order array with the new parts
-        global $failed, $order;
-        for ($x = 0; $x < count($holder); $x++){
-            $order [$failed[$x]] = $holder[$x];
-        }
-        if (verify3($order)===true){
-            var_dump($order);
-            return true;
-        }
-        else{return;}
-    }
-
-    /*
-    $counter=0;
-
-    //Brute force the failed
-    //10P10 is ~3million but the last time I tried this returns 600
-    //Need to remember backtracking
-    function bff ($holder, $carry){
-        if (count($holder) === 10){
-            swapAndVerify($holder);
-            return;
-        }
-        else{
-            foreach ($carry as $y){
-                array_push($holder, $y);
-                $filter = array_filter ($carry, fn ($x) => $x !== $y);
-                bff($holder, $filter);
-                global $counter;
-                $counter++;
+                if ($tickets[$x][$y] >= $rule->lower2 && $tickets[$x][$y]<= $rule->upper2){
+                    array_push($satisfies[$x][$y][$z],"b");
+                }
+                $z++;
             }
         }
     }
-
-    bff(array(), $failed);
-    //Brute forcing ended up with 7million in the counter and none of them made the rules work.
-    */
-
-    //Threw in the towel
-    //Some googling and I'm getting Heap's algorithm: https://en.wikipedia.org/wiki/Heap%27s_algorithm
-
-    $length = count($failed);
-    function heapsalgo ($k, $array){
-        if ($k===1){
-            swapAndVerify($array);
-        }
-        else{
-            heapsalgo ($k-1, $array);
-
-            for ($x = 0; $x< $k-1; $x++){
-                if ($k % 2 === 0){
-                    $temp = $array[$x];
-                    $array[$x]=$array[$k-1];
-                    $array[$k-1]=$temp;
+    //x = ticket, y = entry, z = rules satisfied
+    for ($x = 0; $x<count($satisfies); $x++){
+        for ($y = 0; $y<20; $y++){
+            for ($z = 0; $z< 20; $z++){
+                if (count($satisfies[$x][$y][$z])===0){
+                    printf($x . " ". $y . " ". $z . "\n");
                 }
-                else{
-                    $temp = $array[0];
-                    $array[0]=$array[$k-1];
-                    $array[$k-1]=$temp;
-                }
-
-                heapsalgo ($k-1, $array);
             }
         }
     }
-    heapsalgo($length-1, $failed);
-    //Permutations here don't work.
+    //on every ticket there is at least one entry-rule pair that doesn't work
+    //ticket #27 entry 2 has the most incompatibilities?
+    //it has an entry of 0, that was an oversight
+    //made adjustments on line 160
 
-    //Some tickets have duplicate entries
+    //Your ticket (#0) has the least incompatibilities on its own
 
-    //Reddit thread says greedy, rule-index sieves
+    //Based on the visualization I saw, it kind of looked like it was reducing entries and making some sort of staircase
 
-    //Each entry, write down which rules work for it
+    //For each rule, swap order until 0 incompatible?
+
 
     return;
 ?>
